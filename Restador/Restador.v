9bd59e7bac3799ae9612fa770693a78a3ae00e82
Codys03 <jorgean.barandicaar@ecci.edu.co>
2024-09-06 10:19:45 -0500

    update codigo

--- Restador/Restador.v
+++ Restador/Restador.v
@@ -3,26 +3,25 @@
 module restador(
     input[4:0] minuendo, 
     input[4:0] sustraendo,
-    input [7:0] a2,
     output[4:0] restador
 );
-
+reg [4:0] a2;
 wire c1,c2,c3,c4;
-
+integer i; 
 always(*) begin
+    //calcular el cpmplemento a 1 
    for ( i= 0 ; i<=4 ; i++) begin
-        if (sustraendo[i]=1)begin
+        if (sustraendo[i]==1)begin
             a2[i]=0;
         end
         else begin
             a2[i]=1;
-        end
-    
+        end    
    end 
-
+  
 end
 
-
+a2=a2+1;
 
 
 
