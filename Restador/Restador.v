85efd1d410eb2ad856d59c371e43b6981956f806
Codys03 <jorgean.barandicaar@ecci.edu.co>
2024-09-13 10:55:23 -0500

    res

--- Restador/Restador.v
+++ Restador/Restador.v
@@ -3,18 +3,29 @@
 module Restador(
     input [3:0] A, 
     input [3:0] B,
-    output [3:0] restador,
+    output [3:0] Resultado,  // Resultado en binario positivo
+    output Signo,  // Indicador de signo: 0 = positivo, 1 = negativo
     input  sel,
-    output C_out
+    output C_out  // Carry out
 );
     reg [3:0] resul;
+    wire [3:0] Suma;
     integer i;
+
     always @(*) begin  
+        // Calcular complemento a uno de B si sel es 1
         for (i = 0; i < 4; i = i + 1) begin
             resul[i] = B[i] ^ sel;
         end
     end
 
-    sum4b S (.A(A), .B(resul), .Ci(sel), .Cout(C_out), .Sum(restador)); 
-    
+    // Instanciar el sumador de 4 bits
+    sum4b S (.A(A), .B(resul), .Ci(sel), .Cout(C_out), .Sum(Suma));
+
+    // El signo se determina por el valor de C_out
+    assign Signo = ~C_out;  // Si C_out es 0, significa que hubo borrow, y el resultado es negativo
+
+    // Si el resultado es negativo, obtener el valor absoluto
+    assign Resultado = (Signo) ? (~Suma + 1) : Suma;
+
 endmodule
--- Restador/Restador_tb.v
+++ Restador/Restador_tb.v
@@ -3,11 +3,12 @@
 
 module Restador_tb();
 
-    reg [3:0] A_tb;    // Minuendo, 5 bits
-    reg [3:0] B_tb;    // Sustraendo, 5 bits
+    reg [3:0] A_tb;    // Minuendo, 4 bits
+    reg [3:0] B_tb;    // Sustraendo, 4 bits
   
     wire Cout_tb;
-    wire [3:0] res_tb; // Resultado, 5 bits
+    wire [3:0] res_tb; // Resultado, 4 bits
+    wire signo;
     reg sel_tb;
   
     // Instanciar la Unidad Bajo Prueba (UUT)
@@ -15,24 +16,22 @@ module Restador_tb();
       .A(A_tb), 
       .B(B_tb),  
       .C_out(Cout_tb), 
-      .restador(res_tb),
-       .sel(sel_tb)
+      .Resultado(res_tb),
+      .sel(sel_tb),
+      .Signo(signo)
     );
   
     initial begin
         // Inicializar A_tb y B_tb
         A_tb = 5'b00001; // Inicializa el minuendo en 1
-       // sel_tb=1;
+        sel_tb = 1;
         //Bucle para incrementar A_tb y probar con diferentes valores de B_tb
-        for (A_tb = 1; A_tb < 16; A_tb = A_tb + 1) begin
+        for (A_tb = 1; A_tb < 15; A_tb = A_tb + 1) begin
             for (B_tb = 0; B_tb < 15; B_tb = B_tb + 1) begin
                
-                #2; sel_tb = 1;
-   	            #2; sel_tb = 0;
-	    	    #20
                 #5; // Esperar 5 unidades de tiempo
 
-                $display("El valor de %d - %d = %d", A_tb, B_tb, res_tb);
+                $display("El valor de %d - %d = %d signo= %d", A_tb, B_tb, res_tb, signo);
             end
         end
        
@@ -43,5 +42,7 @@ module Restador_tb();
         $dumpfile("restador_tb.vcd");
         $dumpvars(-1, uut);
     end
-  
+  
 endmodule
+
+  
